02f5c6b40ce4a84fe1f3412fd83c352ab1060721
Joel Small <joel.small@biscon.com.au>
2017-04-17 11:47:34 +0930

    Actual functionality added!

--- UUIDBehavior.php
+++ UUIDBehavior.php
@@ -10,10 +10,37 @@ namespace enigmatix\uuid;
 
 
 use yii\base\Behavior;
+use yii\behaviors\AttributeBehavior;
+use yii\db\BaseActiveRecord;
+use Ramsey\Uuid\Uuid;
 
-
-
-class UUIDBehavior extends Behavior
+class UUIDBehavior extends AttributeBehavior
 {
 
+    public $uuidAttribute = 'uuid';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                BaseActiveRecord::EVENT_BEFORE_INSERT => [$this->uuidAttribute],
+            ];
+        }
+    }
+
+    /**
+     * @inheritdoc*
+     */
+    protected function getValue($event)
+    {
+        if ($this->value === null) {
+            return Uuid::uuid1();
+        }
+        return parent::getValue($event);
+    }
 }
